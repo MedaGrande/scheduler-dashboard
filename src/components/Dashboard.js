feee67f5ec8c91ad15a1131ee1171fc38118d4c4
Medhanie Alene <alenemedhanie@gmail.com>
2023-06-20 22:58:46 -0400

    Implement selectPanel instance method

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,7 +31,12 @@ class Dashboard extends Component {
     loading: false,
     focused: null
   };
-  
+
+  selectPanel(id) {
+  this.setState(previousState => ({
+    focused: previousState.focused !== null ? null : id
+  }));
+}
 
   render() {
     const dashboardClasses = classnames("dashboard", {
@@ -45,9 +50,9 @@ class Dashboard extends Component {
     const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
       <Panel
         key={panel.id}
-        id={panel.id}
         label={panel.label}
         value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
       />
     ));
 
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 class Panel extends Component {
   render() {
-    const { label, value } = this.props;
+    const { label, value, onSelect } = this.props;
    
     return (
       <section
         className="dashboard__panel"
       >
-        <h1 className="dashboard__panel-header">{label}</h1>
+        <h1 className="dashboard__panel-header" onClick={onSelect}>{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
       </section>
     );
